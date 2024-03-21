41ff9dc009edbaf63271a894a2067d2aa5db01ce
JulioRogers <juliorogers.cg@gmail.com>
2024-03-21 10:02:14 -0500

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './platzi.webp';
 import './App.css';
 import { TodoCounter } from './TodoCounter';
@@ -11,17 +12,74 @@ const defaultTodos = [
   {text: 'Cortar cebolla', completed: false},
   {text: 'Tomar el curso de intro a React', completed: false},
   {text: 'Llorar con la llorona', completed: false},
-  {text: 'Llorar con la lloroa', completed: false},];
+  {text: 'Llorar con la lloroa', completed: true},];
+
+
+  function useLocalStorage(itemName, initialValue) {
+
+    const localStorageItem = localStorage.getItem(itemName);
+    let parsedItem;
+
+    if (!localStorageItem) {
+      localStorage.setItem(itemName, JSON.stringify([]));
+      parsedItem = initialValue;
+    } else {
+      parsedItem = JSON.parse(localStorageItem);
+  
+    }
+
+    const [item,setItem]=React.useState(parsedItem);
+
+    const saveItem = (newItem) => {
+      localStorage.setItem(itemName, JSON.stringify(newItem));
+      setItem(newItem);};
+
+    return [
+      item,
+      saveItem,
+    ];
+  
+  }
 
 function App() {
+ 
+
+  //STATES
+  const [todos, saveTodos] =useLocalStorage('todos', []);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+
+  //SEARCH
+  const SearchedTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()))
+
+  //FUNCTIONS FOR BUTTONS
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    saveTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  }
+
+
+
   return (
     <>
-      <TodoCounter completed={16} total={25}/>
-      <TodoSearch/>
+      <TodoCounter completed={completedTodos} total={totalTodos}/>
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
       
       <TodoList>
-        {defaultTodos.map(todo => (
-        <ToDoItem key={todo.text} text={todo.text} completed={todo.completed}/>))}
+        {SearchedTodos.map(todo => (
+        <ToDoItem key={todo.text} text={todo.text} completed={todo.completed} OnComplete={()=>completeTodo(todo.text)} OnDelete={()=>deleteTodo(todo.text)}/>))}
       </TodoList>
 
       <CreateTodoButton/>
@@ -31,3 +89,4 @@ function App() {
 
 
 export default App;
+
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,11 +1,17 @@
 import './ToDoItem.css';
 
-export function ToDoItem({ text, completed }) {
+export function ToDoItem({ text, completed, OnComplete, OnDelete}) {
   return (
     <li className='TodoItem'>
-      <span className={`Icon Icon-check ${completed&&"Icon-check--active"}`}>V</span>
+      <span 
+      className={`Icon Icon-check ${completed&&"Icon-check--active"}`}
+      onClick={OnComplete}
+      >V</span>
+
       <p className={`TodoItem-p ${completed&&"ToDoItem-p--complete"}`}>{text}</p>
-      <span className='Icon Icon-delete'>X</span>
+
+      <span className='Icon Icon-delete' onClick={OnDelete} >X</span>
+
     </li>
   );
 }
